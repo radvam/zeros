2a7543bcc356b328deda5e80851942b3b70d0a5d
Dmitry Radkovets <dmitry.radkovets@gmail.com>
2019-10-09 03:15:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let two = 0; 
+  let five = 0;
+  let ten = 0;
+
+  let arrFactorials = expression.split('*');
+  
+  for (let i = 0; i < arrFactorials.length; i++) {
+
+    if(arrFactorials[i].endsWith("!!")) {
+
+      let n = parseInt(arrFactorials[i]);
+
+      if (n % 2 === 0) {
+        two += amount(n, 2);
+        ten += Math.floor(n / 10) + Math.floor(n / 50);
+      } else {
+        five += amount(n, 5) - Math.floor(n / 10) - Math.floor(n / 50);
+      }
+
+    } else {
+      let n = parseInt(arrFactorials[i]);
+      two += amount(n, 2);
+      five += amount(n, 5);
+    }
+  }
+
+  return Math.min(two, five) + ten;
+} 
+  
+function amount(dividend, divider) {
+  let res = 0;
+  while(dividend > 0) {
+    dividend = Math.floor(dividend / divider);
+    res += dividend;
+  }
+  return res;
 }
